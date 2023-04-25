6b01d7934270edfeea5b64d69cc11d149928f677
Akinpeju Yusuf <akinpejuyusuf@bani.africa>
2023-04-25 05:51:37 +0100

    preload updates

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -12,9 +12,14 @@ export default function Document() {
         />
         <link rel="preload" href="/images/bayo-img.png" as="image" />
         <link rel="preload" href="/images/hero-about.png" as="image" />
-        <link rel="preload" href="/images/pl-hero.png" as="image" />
-        <link rel="preload" href="/images/virtual-account.png" as="image" />
         <link rel="preload" href="/images/payment-link.png" as="image" />
+        <link rel="preload" href="/images/virtual-account.png" as="image" />
+        <link
+          rel="preload"
+          href="/images/virtual-account-hero.png"
+          as="image"
+        />
+        <link rel="preload" href="/images/pl-hero.png" as="image" />
       </Head>
 
       <body>
